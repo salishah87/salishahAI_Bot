e72b983159d30f2fbe874b1afa9bb259c4b36616
seyedali_shahvazi1387 <seyedali.shahvazi@gmail.com>
2025-06-26 16:51:01 +0330

    Delete db directory

--- db/__init__.py
+++
@@ -1,1 +0,0 @@
-# MongoDB integration and database handlers
--- db/connection.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from config import MONGO_URI
-
-client = MongoClient(MONGO_URI)
-db = client["telegram_ai_bot"]
-
-# دسترسی به کالکشن‌ها
-chat_collection = db["chats"]
-user_collection = db["users"]
