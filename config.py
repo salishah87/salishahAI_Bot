38e2870913fb738fa5fc9faff0529df50bcbcb52
seyedali_shahvazi1387 <seyedali.shahvazi@gmail.com>
2025-06-26 16:46:26 +0330

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7884001663:AAFbCqF21mZBBRW0cxC__8sxt5hZ5S7w23w")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://botuser:aiBot2025@cluster0.th9o58e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "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")
